6fdc584eb02fdd0ffcdcc538a4398382845a72d2
Lucas Thomis <lucasthomis@Lucass-MacBook-Pro.local>
2022-03-08 19:27:58 -0500

    updated package.json and server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const cors = require("cors");
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  // origin: 'http://localhost:3000',
   credentials: false,
   optionSuccessStatus: 200
 };
