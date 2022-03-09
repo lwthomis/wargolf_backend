c656676bb3d43747fa64551f4b345ad737bf73c5
Lucas Thomis <lucasthomis@Lucass-MacBook-Pro.local>
2022-03-08 20:42:25 -0500

    added PORT variable to server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require('dotenv').config();
-
+const PORT = process.env.PORT || 3001;
 
 const cors = require("cors");
 const corsOptions = {
